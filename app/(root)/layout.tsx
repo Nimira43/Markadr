c737a14a9236722063a339630ee03caa6949ac49
Nimira43 <n.rabone72@aol.com>
2024-12-21 21:54:24 +0000

    Created (root) folder, created new layout.tsx and brought page.tsx from app folder into it.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className='flex h-screen flex-col'>
+      ROOT
+      <main className="flex-1 wrapper">
+        { children }
+      </main>
+    </div>
+  )
+}

6720c32192f737e0efc8558ce9f6e076546fe2e3
Nimira43 <n.rabone72@aol.com>
2024-12-21 23:13:05 +0000

    Brought in dragon-logo.png as a possible logo and the APP_NAME constant

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import { RiUser3Line } from 'react-icons/ri'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
+import { APP_NAME } from '@/lib/constants'
 
 const Header = () => {
   return ( 

7b3e38a7372fdaf12330e62fd722caefe60d05b4
Nimira43 <n.rabone72@aol.com>
2024-12-21 23:08:42 +0000

    Imports for header index.tsx

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,15 @@
+import { MdOutlineShoppingCart } from 'react-icons/md'
+import { RiUser3Line } from 'react-icons/ri'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+const Header = () => {
+  return ( 
+    <>
+      Header
+    </>
+   )
+}
+ 
+export default Header

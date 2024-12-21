8b3033a044f9b50993eaaa8a2d190fc010541ffa
Nimira43 <n.rabone72@aol.com>
2024-12-21 23:42:44 +0000

    Using Vanilla CSS for app-name logo text. Brought in Dragon logo to header

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -7,9 +7,24 @@ import { APP_NAME } from '@/lib/constants'
 
 const Header = () => {
   return ( 
-    <>
-      Header
-    </>
+    <header className='w-full border-b'>
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href='/' className='flex-start'>
+            <Image 
+              src='/images/dragon-logo.png' 
+              alt={`${APP_NAME} logo`} 
+              height={48} 
+              width={48}
+              priority={true}
+            />
+            <span className='hidden lg:block font-bold text-2xl ml-3 app-name'>
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+      </div>
+    </header>
    )
 }
  

1e2d51cde987034936cb2718b668f31f6a4f156b
Nimira43 <n.rabone72@aol.com>
2024-12-23 21:32:02 +0000

    Using useState and useEffect to help stop Hydration warnings

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -1,4 +1,5 @@
-'use Client'
+'use client'
+import { useState, useEffect } from 'react'
 import { Button } from '@/components/ui/button'
 import { 
   DropdownMenu, 
@@ -12,14 +13,31 @@ import { FiSun, FiMoon } from 'react-icons/fi'
 import { LuSunMoon } from 'react-icons/lu'
 
 const ModeToggle = () => {
+  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return ( 
     <>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant='outline'>
-            <FiSun />
+            { 
+              theme === 'system' ? (
+                <LuSunMoon />
+              ) : theme === 'dark' ? (
+                <FiMoon />
+              ) : (
+                <FiSun />
+              )
+            }
           </Button>
         </DropdownMenuTrigger>
       </DropdownMenu>

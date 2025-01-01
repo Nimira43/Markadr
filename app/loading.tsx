7c85e27b6236b9deaa78af13a5f6930576f74005
Nimira43 <n.rabone72@aol.com>
2025-01-01 23:59:09 +0000

    Positioning the spinner with inine styles

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,13 @@ import loader from '@/assets/loader.gif'
 
 const LoadingPage = () => {
   return ( 
-    <div>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      width: '100vw',
+    }}>
       <Image src={loader} height={150} width={150} alt='Loading...' />     
     </div>
    )

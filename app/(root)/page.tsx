ab5a388ed25fb4978658a4c3aa9db49caf0ccd04
Nimira43 <n.rabone72@aol.com>
2025-01-02 00:05:35 +0000

    Testing loading spinner - all good

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
-const Homepage = () => {
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const Homepage = async () => {
+  await delay(2000)
+  
   return ( 
     <>
       <h1>Home Page</h1>

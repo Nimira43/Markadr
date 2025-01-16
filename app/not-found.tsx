1ff426abd3b2c2bb2883a6369f79e00994a9f18f
Nimira43 <n.rabone72@aol.com>
2025-01-16 11:52:27 +0000

    More styling to the not-found-tsx page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
   return ( 
     <div className='flex flex-col items-center justify-center min-h-screen'>
       <Image 
-        src='/images/dragon-logo.png' 
+        src='/images/logo1.png' 
         width={48} 
         height={48} 
         alt={`${APP_NAME} 
@@ -16,7 +16,7 @@ const NotFoundPage = () => {
       />
       <div className='p-6 w-1/3 rounded-lg shadow-md text-center'>
         <h1 className='text-3xl mb-4 text-[#ff4500]'>Page Not Found</h1>
-        <p className='text-[#111]'>We could not find the page you were looking for</p>
+        <p className='text-[#fffaf0]'>We could not find the page you were looking for</p>
         <Button
           className='mt-4 ml-2 uppercase' 
           onClick={() => {

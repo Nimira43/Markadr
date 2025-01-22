0748883125ce59067aaf3ca171f0b59451e52054
Nimira43 <n.rabone72@aol.com>
2025-01-22 09:52:56 +0000

    Create product-price.tsx

---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,8 @@
+const ProductPrice = ({value, className}: {value: number; className?: string}) => {
+  const stringValue = value.toFixed(2)
+  const [intValue, floatValue] = stringValue.split('.')
+
+  return (  )
+}
+ 
+export default ProductPrice

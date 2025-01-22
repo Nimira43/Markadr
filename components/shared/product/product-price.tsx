1b4dfead0bbbeba1dac95d304f27a5e4a7aecb74
Nimira43 <n.rabone72@aol.com>
2025-01-22 09:59:57 +0000

    ProductPrice component and the utility function cn for combining class name

--- components/shared/product/product-price.tsx
+++ components/shared/product/product-price.tsx
@@ -1,8 +1,18 @@
+import { cn } from '@/lib/utils'
+
 const ProductPrice = ({value, className}: {value: number; className?: string}) => {
   const stringValue = value.toFixed(2)
   const [intValue, floatValue] = stringValue.split('.')
 
-  return (  )
+  return ( 
+    <p 
+      className={ 
+        cn('text-2xl', className)
+      }
+    >
+      <span></span>
+    </p>
+   )
 }
  
 export default ProductPrice

498f0bfc27fe16b83da34a5131774eb2ab0dd9e8
Nimira43 <n.rabone72@aol.com>
2025-02-13 22:39:52 +0000

    Created a const to hold Latest products limit.

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,4 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ||'Markaðr'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION ||'E-commerce website using Next JS 15, TypeScript, Tailwind, Shadcn, Prisma and PostgreSQL'
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4

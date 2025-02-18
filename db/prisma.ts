4effb972f35e3f621b8c64a5f2f8574c4a601f39
Nimira43 <n.rabone72@aol.com>
2025-02-18 17:22:30 +0000

    Extends PrismaClient with custome result transformer to convert price and rating fields to string.

--- db/prisma.ts
+++ db/prisma.ts
@@ -7,3 +7,20 @@ neonConfig.webSocketConstructor = ws
 const connectionString = `${process.env.DATABASE_URL}`
 const pool = new Pool({ connectionString })
 const adapter = new PrismaNeon(pool)
+
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString()
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString()
+        },
+      },
+    }
+  }
+})

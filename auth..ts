f9bced66c902aba551a58908f048840e07d0e58d
Nimira43 <n.rabone72@aol.com>
2025-03-26 21:20:10 +0000

    If password correct return the user.

--- auth..ts
+++ auth..ts
@@ -28,6 +28,15 @@ export const config = {
       })
       if (user && user.password) {
         const isMatch = compareSync(credentials.password as string, user.password)
+        
+        if (isMatch) {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role
+          }
+        }
       }
     }
   })]

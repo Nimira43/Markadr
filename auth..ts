b4d106330669e3f421601790a0d92ea95816bb9a
Nimira43 <n.rabone72@aol.com>
2025-03-26 20:51:10 +0000

    Within the config object setting up pages - first the signIn route,

--- auth..ts
+++ auth..ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth'
 
-export const config = {}
+export const config = {
+  pages: {
+    signIn: '/sign-in',
+  }
+}
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config)

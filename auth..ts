406eb7a01caeb963be1c15803ff2b94173ab59aa
Nimira43 <n.rabone72@aol.com>
2025-03-27 20:39:38 +0000

    Created callbacks object.

--- auth..ts
+++ auth..ts
@@ -14,33 +14,36 @@ export const config = {
     maxAge: 30 * 24 * 60 * 60,
   },
   adapter: PrismaAdapter(prisma),
-  providers: [CredentialsProvider({
-    credentials: {
-      email: { type: 'email'},
-      password: { type: 'password'}
-    },
-    async authorize(credentials) {
-      if (credentials == null) return null
-      const user = await prisma.user.findFirst({
-        where: {
-          email: credentials.email as string
-        }
-      })
-      if (user && user.password) {
-        const isMatch = compareSync(credentials.password as string, user.password)
-        
-        if (isMatch) {
-          return {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            role: user.role
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: 'email'},
+        password: { type: 'password'}
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string
+          }
+        })
+        if (user && user.password) {
+          const isMatch = compareSync(credentials.password as string, user.password)
+          
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role
+            }
           }
         }
+        return null
       }
-      return null
-    }
-  })]
+    })
+  ],
+  callbacks: {}
 }
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config)

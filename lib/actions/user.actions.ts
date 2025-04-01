b1ac1071f74bcca4ce8363fd4cb9ed2ea3f208b6
Nimira43 <n.rabone72@aol.com>
2025-04-01 21:04:05 +0100

    Sign In and Out Action 1

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,3 +4,9 @@ import { signInFormSchema } from '../validators'
 import { signIn, signOut } from '@/auth'
 import { isRedirectError } from 'next/dist/client/components/redirect'
 
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  
+}

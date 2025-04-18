686e9976bc13c102b23dc89ec23b11a8246eed5a
Nimira43 <n.rabone72@aol.com>
2025-04-18 22:18:12 +0100

    Updated AuthLayout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,18 +1,11 @@
-import Header from '@/components/shared/header'
-import Footer from '@/components/footer'
-
-export default function RootLayout({
+export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className='flex h-screen flex-col'>
-      <Header />
-      <main className='flex-1 wrapper'>
-        { children }
-      </main>
-      <Footer />
+    <div className='flex-center min-h-screen w-full'>
+      {children}
     </div>
   )
 }

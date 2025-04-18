ced0a24ce5ef701c3c53a2443a10a5b308835e1f
Nimira43 <n.rabone72@aol.com>
2025-04-18 22:29:00 +0100

    Starting to set up SignInPage card

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,19 @@
+import { Card, CardHeader } from '@/components/ui/card';
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Sign In'
+}
+
 const SignInPage = () => {
   return ( 
-    <>
-      Sign In
-    </>
+    <div className='w-full max-w-md mx-auto'>
+      <Card>
+        <CardHeader className='space-y-4'>
+          
+        </CardHeader>
+      </Card>
+    </div>
    );
 }
  

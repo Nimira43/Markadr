e768bf8facb4abfd4e913a380c0a8b7542bbdda1
Nimira43 <n.rabone72@aol.com>
2025-04-18 22:58:09 +0100

    Set up properties for Image

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,4 +1,5 @@
 import { Card, CardHeader } from '@/components/ui/card';
+import { APP_NAME } from '@/lib/constants';
 import { Metadata } from 'next'
 import Image from 'next/image';
 import Link from 'next/link';
@@ -17,7 +18,8 @@ const SignInPage = () => {
               src='/images/logo1.png'
               width={100}
               height={100}
-              alt={'Markaðr logo'}
+              alt={`${APP_NAME} logo`}
+              priority={true}
             />
           </Link>
         </CardHeader>

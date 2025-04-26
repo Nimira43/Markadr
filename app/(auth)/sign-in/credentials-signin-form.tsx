ce939e2da97f04565447a724fcf760b912eac23b
Nimira43 <n.rabone72@aol.com>
2025-04-26 08:49:55 +0100

    Correcting layout. Link to sign up added.

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -4,40 +4,56 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { signInDefaultValues } from '@/lib/constants'
+import Link from 'next/link'
 
 const CredentialsSignInForm = () => {
   return (  
     <form >
       <div className='space-y-6'>
-        <Label htmlFor='email'>Email</Label>
-        <Input
-          id='email'  
-          name='email'  
-          type='email'
-          required
-          autoComplete='email'  
-          defaultValue={signInDefaultValues.email}
-        />
-      </div>
-      <div className='space-y-6'>
-        <Label htmlFor='password'>Password</Label>
-        <Input
-          id='password'  
-          name='password'  
-          type='password'
-          required
-          autoComplete='password'  
-          defaultValue={signInDefaultValues.password}
-        />
-      </div>
-      <div>
-        <Button
-          className='w-full uppercase'
-          variant='default'
-        >
-          Sign Up
-        </Button>
-      </div>
+        
+        <div>
+          <Label htmlFor='email'>Email</Label>
+          <Input
+            id='email'  
+            name='email'  
+            type='email'
+            required
+            autoComplete='email'  
+            defaultValue={signInDefaultValues.email}
+          />
+        </div>
+        
+        <div>
+          <Label htmlFor='password'>Password</Label>
+          <Input
+            id='password'  
+            name='password'  
+            type='password'
+            required
+            autoComplete='password'  
+            defaultValue={signInDefaultValues.password}
+          />
+        </div>
+
+        <div>
+          <Button
+            className='w-full uppercase'
+            variant='default'
+          >
+            Sign Up
+          </Button>
+        </div>
+        <div className="text-sm text-center text-muted-foreground">
+        Dont&apos;t have an account? {' '}
+          <Link
+            href='/signup'
+            target='_self'
+            className='link'
+          >
+            Sign Up
+          </Link>
+        </div>
+      </div>     
     </form>
   );
 }

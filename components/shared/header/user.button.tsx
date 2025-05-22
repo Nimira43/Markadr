b190fa134a6e57a8370f482e29a88e32fd99ce2e
Nimira43 <n.rabone72@aol.com>
2025-05-22 21:44:50 +0100

    Establishing if user is logged in.

--- components/shared/header/user.button.tsx
+++ components/shared/header/user.button.tsx
@@ -3,8 +3,23 @@ import { auth } from '@/auth'
 import { signOutUser } from '@/lib/actions/user.actions'
 import { Button } from '@/components/ui/button'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { RiUser3Line } from 'react-icons/ri'
 
-const UserButton = () => {
+const UserButton = async () => {
+  const session = await auth()
+
+  if (!session) {
+    return (
+      <Button asChild>
+        <Link href='/sign-in'>
+          <RiUser3Line /> Sign In
+        </Link>
+      </Button>
+    )
+  }
+
+  const firstInitial = session.user?.name?.charAt(0)
+
   return ( 
     <>
       User

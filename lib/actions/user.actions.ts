8939d4729e333a358871ed8db13b446f133db5b1
Nimira43 <n.rabone72@aol.com>
2025-06-03 04:34:51 +0100

    Importing prisma from db/prisma

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { signInFormSchema, signUpFormSchema } from '../validators'
 import { signIn, signOut } from '@/auth'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
 import { hashSync } from 'bcrypt-ts-edge'
+import { prisma } from '@/db/prisma' 
 
 export async function signInWithCredentials(
   prevState: unknown,

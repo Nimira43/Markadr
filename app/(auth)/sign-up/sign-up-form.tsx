dc32be5072c955b3702bb6c0b45c4be7c2816828
Nimira43 <n.rabone72@aol.com>
2025-06-14 09:13:07 +0100

    Cosole logging errors

--- app/(auth)/sign-up/sign-up-form.tsx
+++ app/(auth)/sign-up/sign-up-form.tsx
@@ -46,7 +46,6 @@ const SignUpForm = () => {
             id='name'  
             name='name'  
             type='text'
-            required
             autoComplete='name'  
             defaultValue={signUpDefaultValues.name}
           />
@@ -57,7 +56,6 @@ const SignUpForm = () => {
             id='email'  
             name='email'  
             type='email'
-            required
             autoComplete='email'  
             defaultValue={signUpDefaultValues.email}
           />

45ac617a81bc3f25845f112f86f5b20b80c9da55
Nimira43 <n.rabone72@aol.com>
2025-06-14 09:19:27 +0100

    Creating formatError function

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,3 +16,8 @@ export function formatNumberWithDecimal(num: number): string {
     :
     `${int}.00`
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function formatError(error: any) {
+
+}

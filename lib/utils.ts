e2c888b65f356f8eca248d64ddde6b6b0bd07d14
Nimira43 <n.rabone72@aol.com>
2025-06-14 09:22:39 +0100

    Outlining the formatError function

--- lib/utils.ts
+++ lib/utils.ts
@@ -19,5 +19,11 @@ export function formatNumberWithDecimal(num: number): string {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function formatError(error: any) {
+  if (error.name === 'ZodError') {
 
+  } else if (error.name === 'PrismaClientKnownRequestError' && error.code === 'P2002') {
+
+  } else {
+    
+  }
 }

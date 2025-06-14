d9a8520d1bfa9211af76a7e85029cb1167c32897
Nimira43 <n.rabone72@aol.com>
2025-06-14 09:39:06 +0100

    Handling other errors.

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,10 +22,17 @@ export function formatError(error: any) {
   if (error.name === 'ZodError') {
     const fieldErrors = Object.keys(error.errors).map((field) => error.errors[field].message)
     return fieldErrors.join('. ')
-  } else if (error.name === 'PrismaClientKnownRequestError' && error.code === 'P2002') {
-    const field = error.meta?.target ? error.meta.target[0] : 'Field'
+  } else if (error.name ===   
+    'PrismaClientKnownRequestError' && 
+    error.code === 'P2002'
+  ) {
+    const field = error.meta?.target 
+      ? error.meta.target[0] 
+      : 'Field'
     return `${field.charAt(0).toUppercase() + field.slice(1)} already exists.`
   } else {
-
+    return typeof error.message === 'string' 
+      ? error.message 
+      : JSON.stringify(error.message)
   }
 }

0473dcdbfcd33cf4a98d173f0390aff49e479989
Nimira43 <n.rabone72@aol.com>
2025-07-03 21:10:23 +0100

    Making validators more readable.

--- lib/validators.ts
+++ lib/validators.ts
@@ -9,28 +9,55 @@ const currency = z
   )
 
 export const insertProductSchema = z.object({
-  name: z.string().min(3, 'Name must be at least 3 characters.'),
-  slug: z.string().min(3, 'Slug must be at least 3 characters.'),
-  category: z.string().min(3, 'Category must be at least 3 characters.'),
-  brand: z.string().min(3, 'Brand must be at least 3 characters.'),
-  description: z.string().min(3, 'Description must be at least 3 characters.'),
-  stock: z.coerce.number(),
-  images: z.array(z.string()).min(1, 'Product must have at least 1 image'),
-  isFeatured: z.boolean(),
-  banner: z.string().nullable(),
+  name: z
+    .string()
+    .min(3, 'Name must be at least 3 characters.'),
+  slug: z
+    .string()
+    .min(3, 'Slug must be at least 3 characters.'),
+  category: z
+    .string()
+    .min(3, 'Category must be at least 3 characters.'),
+  brand: z
+    .string()
+    .min(3, 'Brand must be at least 3 characters.'),
+  description: z
+    .string()
+    .min(3, 'Description must be at least 3 characters.'),
+  stock: z.coerce
+    .number(),
+  images: z
+    .array(z.string())
+    .min(1, 'Product must have at least 1 image'),
+  isFeatured: z
+    .boolean(),
+  banner: z
+    .string().nullable(),
   price: currency
 })
 
 export const signInFormSchema = z.object({
-  email: z.string().email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
+  email: z
+    .string()
+      .email('Invalid email address'),
+  password: z
+    .string()
+    .min(6, 'Password must be at least 6 characters'),
 })
 
 export const signUpFormSchema = z.object({
-  name: z.string().min(3, 'Name be at least 3 characters.'),
-  email: z.string().email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
-  confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters'),
+  name: z
+    .string()
+    .min(3, 'Name be at least 3 characters.'),
+  email: z
+    .string()
+    .email('Invalid email address'),
+  password: z
+    .string()
+    .min(6, 'Password must be at least 6 characters'),
+  confirmPassword: z
+    .string()
+    .min(6, 'Confirm password must be at least 6 characters'),
 })
 .refine((data) => data.password === data.confirmPassword, {
   message: 'Password do not match',

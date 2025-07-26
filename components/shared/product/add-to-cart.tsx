a672c17a25f0bec9060641fdebe92b899e1b8856
Nimira43 <n.rabone72@aol.com>
2025-07-26 21:20:49 +0100

    Importing useToast and ToastAction

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -4,6 +4,8 @@ import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
 import { CartItem } from '@/types'
 import { FiPlus } from 'react-icons/fi'
+import { useToast } from '@/hooks/use-toast'
+import { ToastAction } from '@/components/ui/toast'
 
 const AddToCart = ({ item } : {
   item: CartItem 

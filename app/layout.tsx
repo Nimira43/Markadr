c6b021811db1c671444f2cd4a301e596bd05aa90
Nimira43 <n.rabone72@aol.com>
2025-07-26 21:19:17 +0100

    Placed Toaster below children

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>

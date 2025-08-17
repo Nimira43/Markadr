b65f1209d60d49268061f9d897b684c0ba10d98e
Nimira43 <n.rabone72@aol.com>
2025-08-17 07:28:33 +0100

    Set up try catch

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -4,6 +4,13 @@ import { cookies } from 'next/headers'
 import { CartItem } from '@/types'
 
 export async function addItemToCart(data: CartItem) {
+  try {
+    
+  } catch (error) {
+    
+  }
+  
+  
   return {
     success: true,
     message: 'Item added to Cart.'
